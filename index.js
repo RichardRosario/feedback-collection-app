a7d3a36cb6feda8e51e4fe701bed9a1922046f01
unknown <tech2js@gmail.com>
2018-08-09 21:54:36 +0800

    added user model and passport strategy

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { autoIndex: false });
+
 
 const app = express();
 
