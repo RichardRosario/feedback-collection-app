3ac5fb4280e5a57da4bbf66fb0901f1522257c93
unknown <tech2js@gmail.com>
2018-08-18 12:32:29 +0800

    added survey list on client side

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 //refactor with async/await
 export const fetchUser = () => async (dispatch) => {
@@ -8,22 +8,30 @@ export const fetchUser = () => async (dispatch) => {
    dispatch({ type: FETCH_USER, payload: res.data });
   }
 
-  export const handleToken = token => async dispatch => {
-    const res = await axios.post('/api/stripe', token);
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
 
-    dispatch({
-      type: FETCH_USER, payload: res.data
-    });
-  }
+  dispatch({
+    type: FETCH_USER, payload: res.data
+  });
+}
 
-  export const submitSurvey = (values, history) => async dispatch => {
-    const res = await axios.post('/api/surveys', values);
+export const submitSurvey = (values, history) => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
 
-    history.push('/surveys');
-    dispatch({
-      type: FETCH_USER, payload: res.data
-    });
-  }
+  history.push('/surveys');
+  dispatch({
+    type: FETCH_USER, payload: res.data
+  });
+}
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({
+    type: FETCH_SURVEYS, payload: res.data
+  });
+}
 
 
 
